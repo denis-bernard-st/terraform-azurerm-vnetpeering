892918ecac566403602baa161c26d2faf1437813
denis-bernard-st <79141496+denis-bernard-st@users.noreply.github.com>
2022-01-14 01:59:57 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,17 @@
-data "azurerm_subscription" "current" {}
-
-locals {
-  subscription_id_1 = var.allow_cross_subscription_peering ? var.subscription_ids[0] : data.azurerm_subscription.current.subscription_id
-  subscription_id_2 = var.allow_cross_subscription_peering ? var.subscription_ids[1] : data.azurerm_subscription.current.subscription_id
-}
-
-provider "azurerm" {
-  alias           = "sub1"
-  subscription_id = local.subscription_id_1
-  features {}
-}
-
-provider "azurerm" {
-  alias           = "sub2"
-  subscription_id = local.subscription_id_2
-  features {}
-}
-
 data "azurerm_resource_group" "rg1" {
-  provider = azurerm.sub1
   name     = var.resource_group_names[0]
 }
 
 data "azurerm_resource_group" "rg2" {
-  provider = azurerm.sub2
   name     = var.resource_group_names[1]
 }
 
 data "azurerm_virtual_network" "vnet1" {
-  provider            = azurerm.sub1
   name                = var.vnet_names[0]
   resource_group_name = data.azurerm_resource_group.rg1.name
 }
 
 data "azurerm_virtual_network" "vnet2" {
-  provider            = azurerm.sub2
   name                = var.vnet_names[1]
   resource_group_name = data.azurerm_resource_group.rg2.name
 }
